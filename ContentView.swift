d7e6e4d105620cb087390feba1f3c5bc7be4167d
Jashan Kaur Pannu <79289867+jashankaurpannu@users.noreply.github.com>
2022-01-13 18:28:18 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Jashan Kaur on 2021-12-29.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible()),]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.pink).opacity(0.3)
+                                .frame(width: geometry.size.width/3 - 15,
+                                       height: geometry.size.width/3 - 15)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Jashan Kaur on 2021-12-29.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
